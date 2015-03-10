6163b02f6000fd6f431a332e6202c636687ebe61
Wei Song <wsong83@gmail.com>
2015-03-10 17:14:31 +0000

    replace the Berkeley vmlinux with a recompiled one

--- script/test.sh
+++ script/test.sh
@@ -4,9 +4,9 @@ if [[ $cur_path == *"tests/isa"* ]] || [[ $cur_path == *"tests/mt"* ]] || [[ $cu
 then
 	echo "running pre-loaded test cases...";
 	find * -exec echo "*** testing " {} " ***" \; -exec /home/root/fesvr-zynq {} \; -o -exec echo "!!! test case " {} " failed!!!" \; ;
-elif[[ $cur_path == *"tests/tag"* ]]
+elif [[ $cur_path == *"tests/tag"* ]]
 then 
-    echo "running pre-loaded test cases...";
+	echo "running pre-loaded test cases...";
 	find * -exec echo "*** testing " {} " ***" \; -exec /home/root/fesvr-zynq /home/root/pk {} \; -o -exec echo "!!! test case " {} " failed!!!" \; ;
 else
 	echo "wrong directory";
